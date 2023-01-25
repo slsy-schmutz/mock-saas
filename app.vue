426b82b028f5e8a96a8a5d0c9ab5000cef3295c5
Thibaut Schmutz <tschmutz@sellsy.com>
2023-01-25 16:30:22 +0100

    style(app): update main ui

--- app.vue
+++ app.vue
@@ -1,17 +1,45 @@
 <template>
   <div>
-    <div>
+    <div class="nav">
       <a href="https://hono-vue3.tschmutz.workers.dev/individuals/create"
-        >Individual create</a
+        >=> Vue 3 SPA</a
       >
-      <a href="https://hono-vue3.tschmutz.workers.dev/individuals/listing"
-        >Individual listing</a
-      >
+      <a href="https://mock-saas.vercel/app">=> SAAS</a>
     </div>
-    <p>Je suis le SAAS actuel</p>
-    <p>
-      le cloudflare Worker m'a redirigé ici car l'URL n'est pas ouverte à
-      l'application Vue3
-    </p>
+    <div class="content">
+      <h2>Je suis le SAAS actuel</h2>
+      <br />
+      <p>
+        le cloudflare Worker m'a redirigé ici car l'URL demandée n'intègre pas
+        vue3
+      </p>
+    </div>
   </div>
 </template>
+
+<style>
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  min-height: 100vh;
+}
+.nav {
+  width: 100vw;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  background-color: antiquewhite;
+}
+.nav a {
+  display: block;
+  margin-left: 20px;
+}
+.content {
+  padding: 30px;
+}
+</style>
