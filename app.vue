e01c1c8a9a0e9a22b90db2305cc1bdcec747aa64
Thibaut Schmutz <tschmutz@sellsy.com>
2023-01-25 12:05:56 +0100

    feat(route): add home route

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <div>
+      <a href="https://hono-vue3.tschmutz.workers.dev/individuals/create"
+        >Individual create</a
+      >
+      <a href="https://hono-vue3.tschmutz.workers.dev/individuals/listing"
+        >Individual listing</a
+      >
+    </div>
+    <p>Je suis le SAAS actuel</p>
+    <p>
+      le cloudflare Worker m'a redirigé ici car l'URL n'est pas ouverte à
+      l'application Vue3
+    </p>
   </div>
 </template>
